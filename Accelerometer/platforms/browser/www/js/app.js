872b32ca7163b7223bba828b474eb107d906fbba
Vincent <vincentcote87@gmail.com>
2019-03-23 21:39:02 -0600

    Finalized concept

--- Accelerometer/platforms/browser/www/js/app.js
+++ Accelerometer/platforms/browser/www/js/app.js
@@ -5,21 +5,24 @@ if (window.DeviceOrientationEvent) {
 };
 
 let x, y, z;
-let xpos, ypos;
+// let xpos, ypos;
 let r, g, b;
 let myText = "";
 
 function move(e) {
 
-  xpos = map(Number(e.gamma), -90, 90, 0, window.innerWidth, true);
-  ypos = map(Number(e.beta) * 2, -120, 120, 0, window.innerHeight, true);
+  // xpos = map(Number(e.gamma), -90, 90, 0, window.innerWidth, true);
+  // ypos = map(Number(e.beta) * 2, -120, 120, 0, window.innerHeight, true);
+
+ 
+  // console.log(g);
 
   z = e.alpha.toFixed(2);
   x = e.beta.toFixed(2);
   y = e.gamma.toFixed(2);
-  $('#alpha').html(z);
-  $('#beta').html(x);
-  $('#gamma').html(y);
+  // $('#alpha').html(z);
+  // $('#beta').html(x);
+  // $('#gamma').html(y);
 };
 
 function setup() {
@@ -28,33 +31,40 @@ function setup() {
 };
 
 function draw() {
-  background(255, 255, 255);
-  // let opacity = Math.floor(Number(x));
-  // if (opacity >= 30 && opacity <= 60) {
-  //   opacity -= opacity;
-  //   opacity *= 8.5;
-  // } else if (opacity > 60) {
-  //   opacity = 255;
-  // } else
-  //   opacity = 0;
+  // Number(y) < 0 ? r = map(Number(y), -50, 0, 255, 186, true) : r = map(Number(y), 0, 50, 186, 148, true);
+  // g = map(Math.abs(Number(y)), 50, 0, 85, 0, true);
+  // Number(y) < 0 ? b = map(Number(y), -50, 0, 255, 211, true) : b = 211;
+
+  Number(y) < 0 ? r = map(Number(y), -80, 0, 0, 127, true) : r = map(Number(y), 0, 80, 127, 255, true);
+  // g = map(Math.abs(Number(y)), 50, 0, 200, 0, true);
+  g = map(Math.abs(Number(x)), 0, 90, 200, 10, true);
+  Number(y) < 0 ? b = map(Number(y), -80, 0, 255, 127, true) : b = map(Number(y), 0, 80, 127, 0, true);
+
+  background(r, g, b);
   if (Number(x) > 40) {
-    myText = "Hello there"
+    myText = "UP"
   }
-  else if (Number(y) > 20) {
-    myText = "To the left!"
+  if (Number(x) < 40) {
+    myText = "DOWN";
+  } 
+  if (Number(y) > 20) {
+    myText = "RIGHT"
   }
-  else if (Number(y) < -20) {
-    myText = "To the right!"
+  if (Number(y) < -20) {
+    myText = "LEFT"
   }
-  else 
-    myText = "";
-  textSize(60);
+  if (Number(y) < -80) {
+    myText = "DON'T DROP"
+  }
+  if (Number(y) > 55 && Number(y) < 65 && Number(x) > 65) {
+    myText = "JUST RIGHT"
+  }
+
+  textSize(55);
   textAlign(CENTER);
   text(myText, (width / 2), (height / 2));
-  fill(255, 35, 255);
-
-  // xpos = Number(width/2) + Number(y * 5);
-  // ypos = Number(height/2) + Number(x * 8);
-  // ellipse(xpos, ypos, 80, 80);
-  // fill((Number(z * 2)%255), (Number(z * 1.5)%255), (Number(z * 1.35)%255));
+  // text(Math.floor(r), 200, 100);
+  // text(Math.floor(g), 200, 200);
+  // text(Math.floor(b), 200, 300);
+  fill(b, r, g);
 };
